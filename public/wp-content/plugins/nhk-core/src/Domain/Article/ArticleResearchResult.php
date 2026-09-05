18640166af07dc63dbcb5ee714144f5e4e223eef
danhbabieu <danhba.bieu@gmail.com>
2026-09-05 15:09:24 +0700

    feat: expose dictionary plan in article research result

--- public/wp-content/plugins/nhk-core/src/Domain/Article/ArticleResearchResult.php
+++ public/wp-content/plugins/nhk-core/src/Domain/Article/ArticleResearchResult.php
@@ -20,11 +20,12 @@ final readonly class ArticleResearchResult
         public array $blockers,
         public array $warnings,
         public bool $readyForDraft,
+        public array $dictionaryPlan = ['status' => 'NOT_CONFIGURED'],
     ) {}
 
     /** @return array<string,mixed> */
     public function toArray(): array
     {
-        return ['subject_resolution' => $this->subjectResolution, 'inventory' => $this->inventory, 'overlap_analysis' => $this->overlap, 'knowledge_inventory' => $this->knowledgeInventory, 'relation_plan' => $this->relationPlan, 'internal_link_plan' => $this->internalLinks, 'category_plan' => $this->categoryPlan, 'media_plan' => $this->mediaPlan, 'video_plan' => $this->videoPlan, 'seo_blueprint' => $this->seoBlueprint, 'claim_compliance' => $this->compliance, 'blockers' => $this->blockers, 'warnings' => $this->warnings, 'ready_for_draft' => $this->readyForDraft];
+        return ['subject_resolution' => $this->subjectResolution, 'inventory' => $this->inventory, 'overlap_analysis' => $this->overlap, 'knowledge_inventory' => $this->knowledgeInventory, 'relation_plan' => $this->relationPlan, 'internal_link_plan' => $this->internalLinks, 'category_plan' => $this->categoryPlan, 'media_plan' => $this->mediaPlan, 'video_plan' => $this->videoPlan, 'dictionary_plan' => $this->dictionaryPlan, 'seo_blueprint' => $this->seoBlueprint, 'claim_compliance' => $this->compliance, 'blockers' => $this->blockers, 'warnings' => $this->warnings, 'ready_for_draft' => $this->readyForDraft];
     }
 }
