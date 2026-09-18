6a14e3c4a8d71bdd0c79cd1f25ca9c94fc0a553c
Imac 24" - 2125D <imac24-2125d@iMac-cua-Imac.local>
2026-09-18 15:39:57 +0700

    d

--- public/wp-content/plugins/nhk-core/tests/Unit/FrontendPresentationContractTest.php
+++ public/wp-content/plugins/nhk-core/tests/Unit/FrontendPresentationContractTest.php
@@ -33,6 +33,16 @@ final class FrontendPresentationContractTest extends TestCase
         self::assertStringContainsString('$hubLabel', $source);
     }
 
+    public function test_homepage_places_compact_latest_module_in_hero_and_curated_block_after_hero(): void
+    {
+        $source = $this->read('front-page.php');
+        self::assertStringContainsString('class="home-latest-hero"', $source);
+        self::assertStringContainsString('array_slice($home[\'latest\'], 0, 4)', $source);
+        self::assertStringContainsString('id="featured-title"', $source);
+        self::assertLessThan(strpos($source, 'class="home-semantic-section home-hubs"'), strpos($source, 'class="featured-section"'));
+        self::assertLessThan(strpos($source, 'class="hero-media-column"'), strpos($source, 'class="home-latest-hero"'));
+    }
+
     public function test_entity_detail_renders_dossier_knowledge_gallery_and_path_aware_related_content(): void
     {
         $source = $this->read('entity.php');
