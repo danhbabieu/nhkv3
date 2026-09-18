736fdabb96160271896eb2faac841551503f0d23
Imac 24" - 2125D <imac24-2125d@iMac-cua-Imac.local>
2026-09-18 18:15:36 +0700

    d

--- public/wp-content/plugins/nhk-core/tests/Unit/FrontendPresentationContractTest.php
+++ public/wp-content/plugins/nhk-core/tests/Unit/FrontendPresentationContractTest.php
@@ -81,6 +81,7 @@ final class FrontendPresentationContractTest extends TestCase
         self::assertStringNotContainsString('minmax(170px,220px) minmax(0,1fr) minmax(230px,290px)', $source);
         self::assertStringNotContainsString('minmax(160px,190px) minmax(0,1fr) minmax(210px,250px)', $source);
         self::assertStringContainsString("wp_enqueue_style('nhk-v3-presentation', get_theme_file_uri('presentation.css'), ['nhk-v3-knowledge'], '1.0.1')", $this->read('functions.php'));
+        self::assertStringContainsString("if (is_singular('post')) { wp_enqueue_style('nhk-v3-album-style'", $this->read('functions.php'));
     }
 
     public function test_entity_archive_intro_explains_the_reader_purpose_of_each_profile_family(): void
