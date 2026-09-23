de32d3395e94276d68f15806c79ee4caa46d33a6
Imac 24" - 2125D <imac24-2125d@iMac-cua-Imac.local>
2026-09-23 11:35:05 +0700

    chore: remove unrelated subject hierarchy scratch

--- public/wp-content/plugins/nhk-core/src/Application/Semantic/SubjectStructuralContextReader.php
+++
@@ -1,4 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace NHK\Core\Application\Semantic;
-interface SubjectStructuralContextReader { /** @param array<string,mixed> $candidate @return array<string,mixed> */ public function contextFor(array $candidate): array; }
